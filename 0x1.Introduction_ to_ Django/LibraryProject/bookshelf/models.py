b69ea60a769336228485910033483e94f5e557ce
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-08-11 23:49:21 +0300

    admin interface

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200),
-    author = models.CharField(max_length=100),
-    publication_year = models.IntegerField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField(null= False)

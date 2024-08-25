5e706a0f398c38d37f2bf7c396d5f2f7aa8ebf6d
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-08-25 22:07:56 +0300

    Shit

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Book
 from django.utils import timezone
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
@@ -10,3 +11,17 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 # Register your models here.
 #admin.site.register(Book)
+
+
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)

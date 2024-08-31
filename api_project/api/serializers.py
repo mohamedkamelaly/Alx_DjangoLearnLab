23ff473a75015215fc89b9ba5d8e52248c55517e
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-08-31 14:54:03 +0300

    2nd Task in DRF

---
+++ api_project/api/serializers.py
@@ -0,0 +1,7 @@
+from .models import Book
+from rest_framework import serializers
+
+class BookSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .serializers import BookSerializers
+from rest_framework import generics
+from .models import Book
 # Create your views here.
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers

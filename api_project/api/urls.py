759a77e39d2ad5ff534279cd316883a202eb8c7f
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-08-31 15:01:40 +0300

    Second Task Update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
-    path("api/books", BookList.as_view(), name = "Book_create_List")
+    path("books/", BookList.as_view(), name = "Book_create_List")
 
 ]

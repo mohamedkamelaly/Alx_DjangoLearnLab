6bdc9bd24ef965f9740027625270344c49987482
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-09-06 19:50:27 +0300

    #0 Task week #13

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name= 'books')
+

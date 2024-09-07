7c0c14c1ab2f3e98266886ea63ed471997834597
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-09-07 16:59:41 +0300

    1st Task week#13

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"
 from rest_framework.exceptions import ValidationError
 from datetime import datetime
 # Create your views here.

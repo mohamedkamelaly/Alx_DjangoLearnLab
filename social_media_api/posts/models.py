99e0c058fb9433c57624e29cf3adf06a25daecb3
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-09-22 16:27:54 +0300

    Task#1 creation of serializers update_1

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,13 +4,13 @@ from accounts.models import CustomUser
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, name= 'author')
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=250)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post , on_delete=models.CASCADE)
-    content = models.TextField(max_length=250)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
